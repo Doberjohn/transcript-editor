e68f7d85197c014ee9e9611118694fa34f56fd98
John Fanidis <johnfanidis@gmail.com>
2022-09-07 15:59:43 +0300

    Switched to typescript

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="content_center">
+        <p>
+          Welcome to Studio VN21.
+        </p>
+        <span>
+          Page is currently under construction...
+        </span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
