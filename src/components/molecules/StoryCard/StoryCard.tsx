02d391768d1389fc4a928e33fe78da261e0f5064
John Fanidis <johnfanidis@gmail.com>
2022-09-18 01:20:00 +0300

    Fixed layout issues

--- src/components/molecules/StoryCard/StoryCard.tsx
+++ src/components/molecules/StoryCard/StoryCard.tsx
@@ -4,12 +4,11 @@ import {Div, Paragraph, Span} from "../../atoms";
 import {IStoryCard} from "../../../shared/interfaces";
 
 export const StoryCard = ({title, titleSize = 'h6', subtitle, imageUrl, actionUrl, type, ...rest}: IStoryCard) => {
-   const isPreviousCard = type === 'previous';
 
    return (
       <Div {...rest}>
          <a className="card text-decoration-none" href={actionUrl} target="_blank" rel="noreferrer">
-            <img src={imageUrl} className={`card-img-top ${isPreviousCard ? 'previous-card' : ''}`} alt={title}/>
+            <img src={imageUrl} className="card-img-top" alt={title}/>
             <Div className="card-body">
                <Span className={`${titleSize} card-title`}>{title}</Span>
                {subtitle && (
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,40 +8,40 @@ export const Home = () => {
          title: 'Maybe she was lucky',
          titleSize: 'h4',
          subtitle: 'A woman tries desperately to satisfy her husband. Is she going to make it? Is she going to be lucky?',
-         imageUrl: 'https://miro.medium.com/max/700/1*qLx2RbkS1bltPUSpvrDoqA.png',
+         imageUrl: 'https://studio-vn21.s3.eu-central-1.amazonaws.com/short+stories/Maybe+she+was+lucky+Medium.png',
          actionUrl: 'https://blog.studiovn21.com/maybe-she-was-lucky-7a07e2b0ff6f',
          type: 'latest',
       },
       {
          title: 'A guess in the reflection',
-         imageUrl: 'https://miro.medium.com/max/700/1*axxgA1hRaRaqAzPoeS3_6A.png',
+         imageUrl: 'https://studio-vn21.s3.eu-central-1.amazonaws.com/short+stories/A+guess+in+the+reflection+Medium.png',
          actionUrl: 'https://blog.studiovn21.com/auess-in-the-reflection-d5482a65b21f',
          type: 'previous',
       },
       {
          title: 'Newsletter | August 2022',
-         imageUrl: 'https://miro.medium.com/max/700/1*kCN8nEMENjY7e8RBPvxUbA.png',
+         imageUrl: 'https://studio-vn21.s3.eu-central-1.amazonaws.com/newsletter/August+2022+Newsletter.png',
          actionUrl: 'https://medium.com/the-interactive-storytelling/studio-vn21-newsletter-august-2022-9fe02a773061',
          type: 'previous',
       },
       {
          title: 'Newsletter | July 2022',
-         imageUrl: 'https://miro.medium.com/max/700/1*FWCu_rCUqvU8K1wIogzgvg.jpeg',
+         imageUrl: 'https://studio-vn21.s3.eu-central-1.amazonaws.com/newsletter/July+2022+Newsletter.png',
          actionUrl: 'https://medium.com/the-interactive-storytelling/studio-vn21-newsletter-july-2022-116c2e3cfa14',
          type: 'previous',
       },
-      {
-         title: 'Comic | The Storm',
-         imageUrl: 'https://storage.ko-fi.com/cdn/useruploads/display/3561fc86-0690-4932-9f81-44fb5647b478_comicshoplaunchday.png',
-         actionUrl: 'https://ko-fi.com/s/b7280cea07',
-         type: 'previous',
-      },
-      {
-         title: 'Visual Story | The Storm',
-         imageUrl: 'https://storage.ko-fi.com/cdn/useruploads/display/3561fc86-0690-4932-9f81-44fb5647b478_comicshoplaunchday.png',
-         actionUrl: 'https://www.youtube.com/watch?v=BNXrS9r-g5M',
-         type: 'previous',
-      }
+      // {
+      //    title: 'Comic | The Storm',
+      //    imageUrl: 'https://storage.ko-fi.com/cdn/useruploads/display/3561fc86-0690-4932-9f81-44fb5647b478_comicshoplaunchday.png',
+      //    actionUrl: 'https://ko-fi.com/s/b7280cea07',
+      //    type: 'previous',
+      // },
+      // {
+      //    title: 'Visual Story | The Storm',
+      //    imageUrl: 'https://storage.ko-fi.com/cdn/useruploads/display/3561fc86-0690-4932-9f81-44fb5647b478_comicshoplaunchday.png',
+      //    actionUrl: 'https://www.youtube.com/watch?v=BNXrS9r-g5M',
+      //    type: 'previous',
+      // }
    ];
 
    const homePageStories = stories.slice(0, 5);
