31ba5aa79dfc40e25285889124eede63d5878ef6
John Fanidis <johnfanidis@gmail.com>
2022-09-26 21:11:07 +0300

    upgraded to G-Analytics 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import {Div} from "./components/atoms";
 import {Home} from "./pages/Home/Home";
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 const TRACKING_ID = "UA-201203669-2";
 ReactGA.initialize(TRACKING_ID);
 
